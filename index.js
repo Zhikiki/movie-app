1b6d4e1b7b72e7170608ae22898f7e9d30f5f1a8
Zhikiki <zhykinak@gmail.com>
2023-03-14 23:40:16 +0100

    Added localhost 4200 to allowedOrigins

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ const cors = require('cors');
 let allowedOrigins = [
   'http://localhost:8080',
   'http://localhost:1234',
+  'http://localhost:4200',
   'https://myfliks-zhikiki.netlify.app',
 ];
 app.use(
